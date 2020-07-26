977660bde80dc0b68311fcedf8a9b3ec63b10ab1
Gor <Gor633369@mail.ru>
2020-07-26 23:06:19 +0400

    task 6

---
+++ 6 paragraphs/script.js
@@ -0,0 +1,9 @@
+const paragraph = document.querySelector(".paragraph");
+const colorInput = document.querySelector(".color");
+const backColorInput = document.querySelector("#backColor");
+const button = document.querySelector(".btn");
+
+function isValid(value) {}
+colorInput.addEventListener();
+
+//button.addEventListener("click", () => {});
